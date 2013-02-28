526884b0008578fd16ab2851caf2648ee72e83b6
dmitry.korniienko <dmitry.korniienko@akqa.com>
2013-02-28 14:27:53 +0200

    Changed movie structure

--- content/toyota/properties.js
+++ content/toyota/properties.js
@@ -5,21 +5,106 @@ var movieProperties = {
 
 	background : '#171717',
 
+
+
 	parts : {
+		'base' : {
+			name : 'Car base layer',
+			type : 'background',
+			variants : {
+				'base' : {
+					name : 'Car base layer'
+				}
+			}
+		},
+
+		'body' : {
+			name : 'Car body color',
+			type : 'color_overlay',
+			variants : {
+				'white' : {
+					name : 'White color'
+					color_modification : {a : 0, r : 0, g : 0, b : 0}					
+				},
+
+				'red' : {
+					name : 'Red color'
+					color_modification : {a : 0, r : 100, g : 0, b : 0}					
+				},
+
+				'blue' : {
+					name : 'White color'
+					color_modification : {a : 0, r : 0, g : 0, b : 100}					
+				},
+
+				'green' : {
+					name : 'White color'
+					color_modification : {a : 0, r : 0, g : 100, b : 0}					
+				}
+			}
+		},
+
+		'front_logo' : {
+			name : 'Front logo',
+			type : 'solid',
+			variants : {
+				'metallic' : {
+					name : 'Default color'
+					color_modification : {a : 0, r : 0, g : 0, b : 0}			
+				},
+				'blue' : {
+					name : 'White color'
+					color_modification : {a : 0, r : 0, g : 0, b : 100}					
+				}
+			}
+		},
+
+
+		'wheels' : {
+			name : 'Car wheels',
+			type : 'solid',
+			variants : {
+				'default_15in' : {
+					name : 'Default 15" whhels'
+					color_modification : {a : 0, r : 0, g : 0, b : 0}					
+				},
+
+				'luxury_15in' : {
+					name : 'Luxury 15" whhels'
+					color_modification : {a : 0, r : 0, g : 0, b : 0}					
+				}
+			}
+		}
+/*
+ 	parts : {
 		'body' : {
 			name : 'Car body',
+			type : 'solid',
 			variants : {
-				'white' : 'white color',
-				'red' : 'red color'
+				'white' : {
+					name : 'white color'
+				},
+				'red' : {
+					name : 'red color'
+				}
 			}
 		},
 
 		'front_logo' : {
 			name : 'Toyota logo (front)',
+			type : 'color_overlay',
 			variants : {
-				'metallic' : 'in metallic',
+				'metallic' : {
+					color_modification : {a : 0, r : 0, g : 0, b : 0},
+					name : 'in metallic'
+				},
+				'blue' : {
+					color_modification : {a : 0, r : 0, g : 0, b : 100},
+					name : 'blue plastic'
+				},
 				'blue' : 'blue plastic'
 			}
 		}
 	}
+	*/
 }
