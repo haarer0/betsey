0942fcc4b570ea025549a366a8e29253a87ec5ba
dmitry.korniienko <dmitry.korniienko@akqa.com>
2013-03-06 11:59:49 +0200

    Added camera props; removed not used frames from toyota movie

---
+++ content/camera/properties.js
@@ -0,0 +1,18 @@
+var movieProperties = {
+	fileExtensions : 'png',
+	startFromFrame : 0,
+	totalFrames : 16,
+
+	background : '#000',
+
+	parts : {
+		'base' : {
+			name : 'Base layer',
+			variants : {
+				'default' : {
+					name : 'Camera base layer'
+				}
+			}
+		}
+	}
+}
